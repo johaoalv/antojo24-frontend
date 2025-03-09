d1bc2bac9b3cd2c054f748c4d4446985ae205d5e
johaoenoc <johaoalvarado24@gmail.com>
2025-03-09 02:36:34 -0500

    Update axios_base.jsx

--- frontend/src/data/axios_base.jsx
+++ frontend/src/data/axios_base.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:5000/api"; 
+const API_BASE_URL = "https://rapid-food-backend-production.up.railway.app/api"; 
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
