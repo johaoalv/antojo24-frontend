26c1456f85922897459b6524ec7509107584144b
johaoenoc <116662079+johaoenoc@users.noreply.github.com>
2025-03-28 20:31:43 -0500

    Update axios_base.jsx

--- frontend/src/data/axios_base.jsx
+++ frontend/src/data/axios_base.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL_CLOUD = "https://rapid-food-backend-production.up.railway.app/api"; 
 const API_BASE_URL_LOCAL = "http://127.0.0.1:5000/api"
-const API_BASE_BACKEND_LOCAL = "https://a267-186-188-142-25.ngrok-free.app "
+const API_BASE_BACKEND_LOCAL = "https://a267-186-188-142-25.ngrok-free.app/api "
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_BACKEND_LOCAL,
