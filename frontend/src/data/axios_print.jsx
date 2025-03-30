1df9e8ae263ab80d8961ba663e3bdff5cc543f7e
johaoenoc <116662079+johaoenoc@users.noreply.github.com>
2025-03-29 19:42:59 -0500

    Update axios_print.jsx

--- frontend/src/data/axios_print.jsx
+++ frontend/src/data/axios_print.jsx
@@ -1,18 +1,17 @@
-import axiosInstance from "./axios_base";
+
 import axios  from "axios";
 
 const axiosPrint = axios.create({
-  baseUrl: import.meta.env.VITE_API_BASE_URL_NGROK,
+  baseURL: import.meta.env.VITE_API_BASE_URL_NGROK,
   headers: {
     "Content-Type": "application/json",
   },
-
-})
+});
 
 export const imprimirTicket = async (datos) => {
     console.log("enviando datos al backend para impresion:", datos)
   try {
-    const response = await axiosInstance.post("/imprimir-ticket", datos);
+    const response = await axiosPrint.post("/imprimir-ticket", datos);
     return response.data;
   } catch (error) {
     console.error("Error en imprimir ticket:", error);
