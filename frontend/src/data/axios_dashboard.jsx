62c3ae75fa558b9464d7ce726dd37c7fc66a6163
Joha <116662079+johaoenoc@users.noreply.github.com>
2025-04-13 02:30:06 -0500

    no console logs

--- frontend/src/data/axios_dashboard.jsx
+++ frontend/src/data/axios_dashboard.jsx
@@ -3,7 +3,6 @@ import axiosInstance from "./axios_base";
 export const obtenerDashboard = async () => {
   try {
     const response = await axiosInstance.get("/dashboard");
-    console.log("Respuesta de Axios:", response.data);
     return response.data; 
   } catch (error) {
     console.error("Error en obtenerDashboard:", error);
--- frontend/src/data/axios_pedidos.jsx
+++ frontend/src/data/axios_pedidos.jsx
@@ -1,7 +1,6 @@
 import axiosInstance from "./axios_base";
 
 export const enviarPedido = async (datos) => {
-    console.log("recibiendo datos del front:", datos)
   try {
     const response = await axiosInstance.post("/pedido", datos);
     return response.data;
