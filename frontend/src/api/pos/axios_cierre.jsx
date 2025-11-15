ba8a39d3de820e1d843fa38e52b8847660b415ad
johaoalv <129918127+johaoalv@users.noreply.github.com>
2025-11-15 00:05:16 -0500

    Update axios_cierre.jsx

--- frontend/src/api/pos/axios_cierre.jsx
+++ frontend/src/api/pos/axios_cierre.jsx
@@ -1,7 +1,7 @@
 import axiosInstance from "../core/axios_base";
 
 export const getResumenVentas = async () => {
-  const sucursal_id = localStorage.getItem("sucursal_id");
+  const sucursal_id = localStorage.getItem("sucur22sal_id");
   const response = await axiosInstance.get(
     `/pedidos-hoy?sucursal_id=${sucursal_id}`
   );
@@ -10,8 +10,8 @@ export const getResumenVentas = async () => {
 };
 
 export const hacerCierreCaja = async (total_real) => {
-  const sucursal_id = localStorage.getItem("sucursal_id");
-  const creado_por = sucursal_id.replace("sucursal_", "");
+  const sucursal_id = localStorage.getItem("sucu22rsal_id");
+  const creado_por = sucursal_id.replace("sucurs33al_", "");
   const payload = { sucursal_id, creado_por, total_real };
   console.log("payload",payload);
   const response = await axiosInstance.post("/cierre-caja", payload);
